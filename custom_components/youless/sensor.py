b8e0622158540880a2391d232aa598b57ec5573d
rkoebrugge <rutger@koebrugge.eu>
2020-04-13 18:49:42 +0200

    Update sensor.py
    Change support from YouLess LS120 to LS110, which doesn't have a RJ11 port to read the Smart meters' P1 port.

--- custom_components/youless/sensor.py
+++ custom_components/youless/sensor.py
@@ -1,18 +1,21 @@
 """
-@ Author      : Gerben Jongerius, Paul de Wit, Robin Harmsen
-@ Date        : 04/29/2018, 11/01/2018, 04/29/2019
-@ Description : Youless Sensor - Monitor power consumption. 
+@ Author      : Rutger Koebrugge, Gerben Jongerius, Paul de Wit, Robin Harmsen
+@ Date        : 04/13/2020, 04/29/2018, 11/01/2018, 04/29/2019
+@ Description : Youless Sensor - Monitor power consumption. This component will add the following sensors
+                   - Current power consumption (in W)
+                   - Current tick count, since the Youless meter started running (in kWh)
 """
-VERSION = '2.0.1'
+VERSION = '2.0.2'
 
 import json
 import logging
+import time
 from datetime import timedelta
 from urllib.request import urlopen
 
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
-from homeassistant.const import CONF_MONITORED_VARIABLES
+from homeassistant.const import CONF_MONITORED_CONDITIONS
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 
@@ -24,34 +27,27 @@ _LOGGER = logging.getLogger(__name__)
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
-        vol.Required(CONF_HOST): cv.string,
-        vol.Optional(CONF_MONITORED_VARIABLES, default=['pwr', 'net']): vol.All(
-            cv.ensure_list, vol.Length(min=1), [vol.In(['pwr', 'net', 'p1', 'p2', 'n1', 'n2', 'cs0', 'ps0', 'gas'])])
+        vol.Optional(CONF_HOST): cv.string,
+        vol.Optional(CONF_MONITORED_CONDITIONS, default=['pwr', 'cnt']): vol.All(
+            cv.ensure_list, vol.Length(min=1), [vol.In(['pwr', 'cnt'])])
     })
 }, extra=vol.ALLOW_EXTRA)
 
 SENSOR_TYPES = {
-    'pwr': ['Current Power usage', 'current_power_usage', 'W', 'mdi:flash', 'energy.png'],
-    'net': ['Net Power usage', 'net_power_meter', 'kWh', 'mdi:gauge', 'electric-meter.png'],
-    'p1': ['Power Meter Low', 'power_meter_low', 'kWh', 'mdi:gauge', 'energy.png'],
-    'p2': ['Power Meter High', 'power_meter_high', 'kWh', 'mdi:gauge', 'energy.png'],
-    'n1': ['Power Delivery Low', 'power_delivery_low', 'kWh', 'mdi:gauge', 'energy.png'],
-    'n2': ['Power Delivery High', 'power_delivery_high', 'kWh', 'mdi:gauge', 'energy.png'],
-    'cs0': ['Power Meter Extra', 'power_meter_extra', 'kWh', 'mdi:gauge', 'energy.png'],
-    'ps0': ['Current Power usage Extra', 'current_power_usage_extra', 'W', 'mdi:flash', 'energy.png'],
-    'gas': ['Gas consumption', 'gas_meter', 'm3', 'mdi:gas-cylinder', 'electric-meter.png']
+    'pwr': ['Power Consumption', 'usage', 'W', 'mdi:flash'],
+    'cnt': ['Meter', 'meter', 'kWh', 'mdi:gauge']
 }
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     host = config.get(CONF_HOST)
-    sensors = config.get(CONF_MONITORED_VARIABLES)
+    sensors = config.get(CONF_MONITORED_CONDITIONS)
     data_bridge = YoulessDataBridge(host)
 
     devices = []
     for sensor in sensors:
         sensor_config = SENSOR_TYPES[sensor]
-        devices.append(YoulessSensor(data_bridge, sensor_config[0], sensor, sensor_config[1], sensor_config[2], sensor_config[3], sensor_config[4]))
+        devices.append(YoulessSensor(data_bridge, sensor_config[0], sensor, sensor_config[1], sensor_config[2], sensor_config[3]))
 
     add_devices(devices)
 
@@ -59,7 +55,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class YoulessDataBridge(object):
 
     def __init__(self, host):
-        self._url = 'http://' + host + '/e'
+        self._url = 'http://' + host + '/a?f=j'
         self._data = None
 
     def data(self):
@@ -68,17 +64,16 @@ class YoulessDataBridge(object):
     @Throttle(timedelta(seconds=1))
     def update(self):
         raw_res = urlopen(self._url)
-        self._data = json.loads(raw_res.read().decode('utf-8'))[0]
+        self._data = json.loads(raw_res.read().decode('utf-8'))
 
 
 class YoulessSensor(Entity):
 
-    def __init__(self, data_bridge, name, prpt, sensor_id, uom, icon, image_uri):
+    def __init__(self, data_bridge, name, prpt, sensor_id, uom, icon):
         self._state = None
         self._name = name
         self._property = prpt
         self._icon = icon
-        #self._image = image_uri
         self._uom = uom
         self._data_bridge = data_bridge
         self.entity_id = 'sensor.' + SENSOR_PREFIX + sensor_id
@@ -92,10 +87,6 @@ class YoulessSensor(Entity):
     def icon(self):
         return self._icon
 
-    #@property
-    #def entity_picture(self):
-    #    return '/local/youless/' + self._image
-
     @property
     def unit_of_measurement(self):
         return self._uom
@@ -108,7 +99,7 @@ class YoulessSensor(Entity):
     def state_attributes(self):
         if self._raw is not None:
             return {
-                'timestamp': self._raw['tm']
+                'timestamp': int(time.time())
             }
 
     def update(self):
@@ -116,3 +107,4 @@ class YoulessSensor(Entity):
         self._raw = self._data_bridge.data()
         if self._raw is not None:
             self._state = self._raw[self._property]
+
